6bf0eb95fe6859ae76c0307f7392d4f1096329b0
Raza <137423353+khalid-raza03@users.noreply.github.com>
2024-06-16 22:11:38 +0530

    first commit

---
+++ HOME_PAGE/home.js
@@ -0,0 +1,35 @@
+//script for moving the slides automatically
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2500); // Change image every 2 seconds
+}
+
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+  const body = document.body;
+  const navbar = document.querySelector('.wrapper');
+  const navbarLinks = document.querySelectorAll('.wrapper a');
+
+  body.classList.toggle('dark-mode');
+  wrapper.classList.toggle('dark-mode');
+  
+  for (let link of navbarLinks) {
+      link.classList.toggle('dark-mode');
+  }
+}
